a49f8f2a9326e40db6e908e40f27453da5412da8
zolotovdy <zolotovdy@ugpa.ru>
2021-08-14 18:15:14 +0300

    Revert "added project files"
    This reverts commit 6eb5b65a1c1b2856217f9e6c6e32103b2376583a.

--- ImageGens/run.py
+++
@@ -1,9 +0,0 @@
-from ini_config import MyIniCfg
-from img_generator import ImgGenerator
-
-def main():
-    cfg = MyIniCfg("image")
-    imgGenerator = ImgGenerator(cfg)
-    imgGenerator.generate()
-
-main()
